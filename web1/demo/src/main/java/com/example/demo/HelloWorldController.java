0e56aa303592fcf16b3c9e4df9ad60bed7a6df2e
fullstack290623 <fullstack290623@gmail.com>
2024-02-22 21:29:32 +0200

    .

---
+++ web1/demo/src/main/java/com/example/demo/HelloWorldController.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldController {
+
+    @GetMapping("/hello")
+    public String helloWorld() {
+        return "<h1 style='color:green'>Hello World!</h1>";
+    }
+}
